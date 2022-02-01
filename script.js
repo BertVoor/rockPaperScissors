4606b7c66a35569d18f39280431905d1b8b9e0fc
Bert <voorjansbert@gmail.com>
2022-02-01 18:37:07 +0100

    Improved and simplified  code, added game funtion to play 5 consecutive games

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const max = 3;
-let computerInt = getRandomInt(max);
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+// computerPlay randomly returns "Rock", "Paper" or "Scissors"
+    
+function computerPlay() {
 
-function getRandomInt(max){
-    return Math.floor(Math.random()*max);
-}
-function computerPlay(){
-    if (computerInt == 0) {
-        return "Rock";      
-    } 
+    // random number 0-1-2
+    function getComputerInt(){
+        return Math.floor(Math.random() * 3);
+    }
+    const computerInt = getComputerInt();
+    
+    // convert number to string "Rock"-"Paper"-"Scissors"
+    if (computerInt == 0){
+        return "Rock";
+    }
     else if (computerInt == 1){
         return "Paper";
     }
@@ -17,19 +19,62 @@ function computerPlay(){
         return "Scissors";
     }
 }
+let computerSelection;
+let playerSelection;
 
+// play a single round and return a string that declares the winner of the round
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == "rock" && computerSelection == "Scissors"){
-        return "You Win! Rock beats Scissors";
+
+    playerSelection = prompt("Rock, Paper or Scissors?");
+    computerSelection = computerPlay();
+
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+        return "It's a TIE, try again...";
     }
-    else if (playerSelection == "rock" && computerSelection == "Paper"){
-        return "You Lose! Paper beats Rock";
+    else if (playerSelection.toLowerCase() == "rock"){
+        if (computerSelection == "Scissors"){
+            return "Rock beats Scissors, You WIN!";
+        }
+        else if (computerSelection == "Paper"){
+            return "Paper beats Rock, You LOSE!";
+        }
     }
-    else if (playerSelection == "rock" && computerSelection == "Rock"){
-        return "It is a Tie! Try again";
+    else if (playerSelection.toLowerCase() == "paper"){
+        if (computerSelection == "Scissors"){
+            return "Scissors beats Paper, You LOSE!";
+        }
+        else if (computerSelection == "Rock"){
+            return "Paper beats Rock, You WIN!";
+        }
+    }
+    else if (playerSelection.toLowerCase() == "scissors"){
+        if (computerSelection == "Rock"){
+            return "Rock beats Scissors, You LOSE!";
+        }
+        else if (computerSelection == "Paper"){
+            return "Scissors beats Paper, You WIN!";
+        }
     }
     else {
-        return "the end";
+        return "Something went wrong, try again.";
     }
 }
 
+// function game() that plays 5 rounds, keeps score and reports a winner at the end
+
+let userScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
+function game() {
+    
+    console.log(playRound());
+
+    console.log(playRound());
+    
+    console.log(playRound());
+
+    console.log(playRound());
+
+    console.log(playRound());
+}
