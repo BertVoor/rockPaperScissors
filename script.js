da88861bb89de4c4925b799f6674d554b11b7b56
Bert <voorjansbert@gmail.com>
2022-02-13 13:56:30 +0100

    finished game, playable in console - no UI

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let computerSelection;
 let playerSelection;
 
 // play a single round and return a string that declares the winner of the round
-function playRound(playerSelection, computerSelection){
+/*function playRound(playerSelection, computerSelection){
 
     playerSelection = prompt("Rock, Paper or Scissors?");
     computerSelection = computerPlay();
@@ -59,22 +59,85 @@ function playRound(playerSelection, computerSelection){
         return "Something went wrong, try again.";
     }
 }
-
+*/
 // function game() that plays 5 rounds, keeps score and reports a winner at the end
 
-let userScore = 0;
-let computerScore = 0;
-let roundNumber = 0;
 
+
+
+    
 function game() {
     
-    console.log(playRound());
+    let userScore = 0;
+    let computerScore = 0;
 
+    for (let i = 0; i < 5; i++){
+
+        
+        let roundNumber = (i + 1);
+        console.log('Round ' + roundNumber);
+        function playRound(playerSelection, computerSelection){
+
+            playerSelection = prompt("Rock, Paper or Scissors?");
+            computerSelection = computerPlay();
+        
+            if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+                return "It's a TIE, try again...";
+            }
+            else if (playerSelection.toLowerCase() == "rock"){
+                if (computerSelection == "Scissors"){
+                    userScore++
+                    return "Rock beats Scissors, You WIN!";
+                    
+                }
+                else if (computerSelection == "Paper"){
+                    computerScore++
+                    return "Paper beats Rock, You LOSE!";
+                    
+                }
+            }
+            else if (playerSelection.toLowerCase() == "paper"){
+                if (computerSelection == "Scissors"){
+                    computerScore++
+                    return "Scissors beats Paper, You LOSE!";
+                    
+                }
+                else if (computerSelection == "Rock"){
+                    userScore++
+                    return "Paper beats Rock, You WIN!";
+                    
+                }
+            }
+            else if (playerSelection.toLowerCase() == "scissors"){
+                if (computerSelection == "Rock"){
+                    computerScore++
+                    return "Rock beats Scissors, You LOSE!";
+                    
+                }
+                else if (computerSelection == "Paper"){
+                    userScore++
+                    return "Scissors beats Paper, You WIN!";
+                    
+                }
+            }
+            else {
+                return "Something went wrong, try again.";
+            }
+        }
+   
     console.log(playRound());
     
-    console.log(playRound());
+    console.log('User score: ' + userScore);
+    console.log('Computer score: ' + computerScore);
+}
 
-    console.log(playRound());
-
-    console.log(playRound());
+if (userScore > computerScore) {
+    return "You scored " + userScore + " points, the Computer scored " + computerScore + " points. YOU WIN!";
+}
+else if (computerScore > userScore) {
+    return "You scored " + userScore + " points, the Computer scored " + computerScore + " points. YOU LOSE...";
+}
+else {
+    return "You scored " + userScore + " points, the Computer scored " + computerScore + " points. IT'S A TIE!";
+}
 }
